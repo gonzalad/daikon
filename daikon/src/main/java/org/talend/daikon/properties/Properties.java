2257aef7b414a63ca213f8112ef59859d36de436
Dmytro Chmyga <dmytro.chmyga@synapse.com>
2017-09-15 10:18:53 +0300

    feat(TCOMP-562): fix formatting

--- daikon/src/main/java/org/talend/daikon/properties/Properties.java
+++ daikon/src/main/java/org/talend/daikon/properties/Properties.java
@@ -230,13 +230,13 @@ public interface Properties extends AnyProperty, ToStringIndent {
      */
     ValidationResult getValidationResult();
 
-
     /**
      * Returns the {@link ValidationResults} for the component properties with errors and warnings when they occurs if added.
      *
      * @return a ValidationResult
      */
     ValidationResults getValidationResults();
+
     /**
      * This goes through all nested properties recursively and replaces them with the newValueProperties given as
      * parameters as long as they are assignable to the Properties type. <br/>
